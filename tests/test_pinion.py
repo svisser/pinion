cd4431ab82a95dc4bc45226b597a0d0c42aab4ae
Simeon Visser <simeon87@gmail.com>
2015-04-26 03:42:58 +0100

    Refactored test cases to use functions

--- tests/test_pinion.py
+++ tests/test_pinion.py
@@ -1,17 +1,20 @@
 import unittest
 
 import pinion
+from pinion import parse_hosts, create_packet
 
+
+class BaseTestCase(unittest.TestCase):
+
+    def test_parse_hosts(self):
+        expected = [("127.0.0.1", 4730)]
+        self.assertEqual(parse_hosts([("127.0.0.1", "4730")]), expected)
+        self.assertEqual(parse_hosts(["127.0.0.1"]), expected)
+        self.assertEqual(parse_hosts(["127.0.0.1:4730"]), expected)
+        self.assertEqual(parse_hosts([{'host': "127.0.0.1"}]), expected)
+        self.assertEqual(parse_hosts([{'host': "127.0.0.1", 'port': "4730"}]), expected)
 
 class GearmanClientTestCase(unittest.TestCase):
-
-    def test_client_accepts_hosts(self):
-        expected = [("127.0.0.1", 4730)]
-        self.assertEqual(pinion.GearmanClient([("127.0.0.1", "4730")]).hosts, expected)
-        self.assertEqual(pinion.GearmanClient(["127.0.0.1"]).hosts, expected)
-        self.assertEqual(pinion.GearmanClient(["127.0.0.1:4730"]).hosts, expected)
-        self.assertEqual(pinion.GearmanClient([{'host': "127.0.0.1"}]).hosts, expected)
-        self.assertEqual(pinion.GearmanClient([{'host': "127.0.0.1", 'port': "4730"}]).hosts, expected)
 
     def test_submit_job_job_data_does_not_contain_null(self):
         client = pinion.GearmanClient(["127.0.0.1"])
