6d40dda6f62c862ca0253a1017fbf05a3d75c0b8
Simeon Visser <simeon87@gmail.com>
2015-04-26 01:31:01 +0100

    Expanded test case for parsing hosts

--- tests/test_pinion.py
+++ tests/test_pinion.py
@@ -7,8 +7,9 @@ class GearmanClientTestCase(unittest.TestCase):
 
     def test_client_accepts_hosts(self):
         expected = [("127.0.0.1", 4730)]
+        self.assertEqual(pinion.GearmanClient([("127.0.0.1", "4730")]).hosts, expected)
         self.assertEqual(pinion.GearmanClient(["127.0.0.1"]).hosts, expected)
-        self.assertEqual(pinion.GearmanClient(["127.0.0.1"]).hosts, expected)
+        self.assertEqual(pinion.GearmanClient(["127.0.0.1:4730"]).hosts, expected)
 
 
 class GearmanWorkerTestCase(unittest.TestCase):
